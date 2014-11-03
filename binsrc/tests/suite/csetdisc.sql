7871a3854b37be69dabda0cdac2fbbf11f0aa46d
Patrick Westphal <patrick.westphal@teebeutelstoerche.de>
2014-11-03 22:00:15 +0100

    added a dummy create cset table function

--- binsrc/tests/suite/csetdisc.sql
+++ binsrc/tests/suite/csetdisc.sql
@@ -35,3 +35,49 @@ CREATE FUNCTION dbg_iri_vec_to_str(in vec any) {
     return res;
 }
 
+CREATE TABLE tmp_row_number (nr int);
+INSERT INTO tmp_row_number VALUES(1);
+
+CREATE FUNCTION create_cset_tbls(in vec any) {
+    declare len, ctr integer;
+    declare create_tbl_str, tmp_col_name varchar;
+    declare tmp_col_name_len integer;
+    declare tbl_counter, new_tbl_counter integer;
+
+    for SELECT TOP 1 nr FROM tmp_row_number
+    do tbl_counter := nr;
+    new_tbl_counter := tbl_counter+1;
+
+    DELETE FROM tmp_row_number;
+    INSERT INTO tmp_row_number VALUES(new_tbl_counter);
+
+    vec := deserialize(vec);
+    len := length(vec);
+    ctr := 0;
+    create_tbl_str := concat('CREATE TABLE cset', tbl_counter, ' (s iri_id_8, ');
+    -- TODO: consider graphs
+
+    while (ctr < len) {
+        -- column name
+        tmp_col_name := concat('' , vec[ctr]);
+        tmp_col_name_len := length(tmp_col_name);
+        create_tbl_str := concat(create_tbl_str, ' tbl', substring(tmp_col_name, 2, tmp_col_name_len-1), ' ');
+        -- column type
+        create_tbl_str := concat(create_tbl_str, ' ANY');
+
+        ctr := ctr+1;
+        -- trailing comma or closing parens
+        if (ctr < len) {
+            create_tbl_str := concat(create_tbl_str, ', ');
+        } else {
+            create_tbl_str := concat(create_tbl_str, ');');
+        }
+    }
+
+    -- looked for something like eval(create_tbl_str) but didn't find anything
+    return create_tbl_str;
+}
+
+SELECT dbg_iri_vec_to_str(cc_ps), cnt FROM cset_cand WHERE cnt > 10 AND length(deserialize(cc_ps))> 1;
+
+SELECT create_cset_tbls(cc_ps) FROM cset_cand WHERE cnt > 10 AND length(deserialize(cc_ps))> 1;
